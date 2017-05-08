7dd1cddebcf2ace2429d38443520142802768be8
pselle <pamela.selle@gmail.com>
2017-05-08 16:35:40 -0400

    Modify formdataness

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
-  document.getElementById('imageForm').addEventListener('submit', function(e) {
+  var form = document.getElementById('imageForm');
+  form.addEventListener('submit', function(e) {
     e.preventDefault();
-    var fd = new FormData();
-    fd.append('imageFile', document.getElementById('imageUpload').files[0]);
+    var fd = new FormData(form);
     fetch("https://31w7u6bill.execute-api.us-east-1.amazonaws.com/prod/selfie/create", {
       method: "POST",
       body: fd
