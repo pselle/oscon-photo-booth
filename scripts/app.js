6bed48481775f39e337b6de1e81fc5a89b8da840
pselle <pamela.selle@gmail.com>
2017-05-08 14:54:33 -0400

    Update form header

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,9 @@
     fd.append('imageFile', document.getElementById('imageUpload').files[0]);
     var http = new XMLHttpRequest();
     http.open('POST', 'https://ixxjg97wd7.execute-api.us-east-1.amazonaws.com/dev/selfie/create', true);
-    http.onload = function() {
-      if (http.status == 200) {
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.onreadystatechange = function() {
+      if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
         alert("Form Submitted");
       }
     };
