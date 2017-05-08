b8c88bfd46a30ad002b190e4cc8c71b8e417d1b4
pselle <pamela.selle@gmail.com>
2017-05-08 16:11:54 -0400

    Try fetch

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,10 @@
     e.preventDefault();
     var fd = new FormData();
     fd.append('imageFile', document.getElementById('imageUpload').files[0]);
-    var http = new XMLHttpRequest();
-    http.open('POST', 'https://aw6eh60gra.execute-api.us-east-1.amazonaws.com/prod/selfie/create', true);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.onreadystatechange = function() {
-      if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
-        alert("Form Submitted");
-      }
-    };
-    http.send(fd);
+    fetch("https://31w7u6bill.execute-api.us-east-1.amazonaws.com/prod/selfie/create", {
+      method: "POST",
+      body: fd
+    });
     console.log(e);
   });
 })();
