b7729349c2dcfcd44a58ef3c228c64d349d14dc8
pselle <pamela.selle@gmail.com>
2017-05-08 14:46:31 -0400

    point at lambda

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,14 @@
     e.preventDefault();
     var fd = new FormData();
     fd.append('imageFile', document.getElementById('imageUpload').files[0]);
-//     var xhr = new XMLHttpRequest;
-// xhr.open('POST', '/', true);
-// xhr.send(fd);
+    var http = new XMLHttpRequest();
+    http.open('POST', 'https://ixxjg97wd7.execute-api.us-east-1.amazonaws.com/dev/selfie/create', true);
+    http.onload = function() {
+      if (http.status == 200) {
+        alert("Form Submitted");
+      }
+    };
+    http.send(fd);
     console.log(data);
     console.log(e);
   });
