e3d5ec18456438ac52f6d20b9618fe5000806436
pselle <pamela.selle@gmail.com>
2017-05-09 15:15:21 -0500

    Re-get form

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 (function() {
   'use strict';
-  var form = document.getElementById('imageForm');
-  form.addEventListener('submit', function(e) {
+  document.getElementById('imageForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    var fd = new FormData(form);
+    var fd = new FormData(document.getElementById('imageForm'));
     fetch("https://31w7u6bill.execute-api.us-east-1.amazonaws.com/prod/selfie/create", {
       method: "POST",
       body: fd
