aea6a0674407ce4d8e793148584cb16db8e23387
pselle <pamela.selle@gmail.com>
2017-05-09 19:52:55 -0500

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,24 @@
   'use strict';
   document.getElementById('imageForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    var fd = new FormData(document.getElementById('imageForm'));
-    fetch("https://31w7u6bill.execute-api.us-east-1.amazonaws.com/prod/selfie/create", {
-      method: "POST",
-      body: fd
-    }).then(function(res) {
-      console.log(res);
-    }).catch(function(err) {
-      console.log(err);
-    });
-    console.log(e);
+    var fd = new FormData();
+    fd.append('imageFile', document.getElementById('imageUpload').files[0]);
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+
+    reader.addEventListener("load", function () {
+      fetch("https://31w7u6bill.execute-api.us-east-1.amazonaws.com/prod/selfie/new", {
+        method: "POST",
+        body: reader.result
+      }).then(function(res) {
+        document.getElementById('selfieResult').src = res.body.message;
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }, false);
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   });
 })();
